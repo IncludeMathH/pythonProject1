27153818c1b2490034f60c69f42a5c713e3cd3ab
Admin <798511305@qq.com>
2022-06-26 23:00:35 +0800

    Changes

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# let us run this cell only if CUDA is available
+# We will use ``torch.device`` objects to move tensors in and out of GPU
+import torch
+
+x = torch.randn(4, 4)
+if torch.cuda.is_available():
+    device = torch.device("cuda")          # a CUDA device object
+    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
+    x = x.to(device)                       # or just use strings ``.to("cuda")``
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('test')
